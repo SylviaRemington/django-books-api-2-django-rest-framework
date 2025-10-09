c4bdceffd6c3b91e19bf2b4715bed608a466620e
SylviaRemington <sylviaremington@gmail.com>
2025-10-09 01:09:51 -0700

    addtl - just finished post create request in postman again

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,8 @@
 # Importing views and path for the index/list view
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView #, BookDetailView
 
 urlpatterns = [
   path('', BookListView.as_view()),
-  path('<int:pk>/', BookDetailView.as_view()),
+  # path('<int:pk>/', BookDetailView.as_view()),
 ]
