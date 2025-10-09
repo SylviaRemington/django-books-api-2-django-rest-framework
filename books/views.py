c93399e5b34c376ad80ed9cf3f8daa979f38ec96
SylviaRemington <sylviaremington@gmail.com>
2025-10-09 01:36:41 -0700

    correcting code

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-# from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound
 
 from .models import Book
 from .serializers import BookSerializer
@@ -21,7 +21,6 @@ class BookListView(APIView):
         # return the serialized data and a 200 status code
         return Response(serialized_books.data, status=status.HTTP_200_OK)
     
-    
     def post(self, request):
         request.data["owner"] = request.user.id
         book_to_add = BookSerializer(data=request.data)
@@ -37,42 +36,42 @@ class BookListView(APIView):
             return Response(e.__dict__ if e.__dict__ else str(e), status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
-# class BookDetailView(APIView):
-#     permission_classes = (IsAuthenticatedOrReadOnly, ) # sets the permission levels of the specific view by passing in the rest framework authentication class
+class BookDetailView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly, ) # sets the permission levels of the specific view by passing in the rest framework authentication class
 
-#     # custom method to retrieve a book from the DB and error if it's not found
-#     def get_book(self, pk):
-#         try:
-#             return Book.objects.get(pk=pk)
-#         except Book.DoesNotExist:
-#             raise NotFound(detail="Can't find that book") # <-- import the NotFound exception from rest_framwork.exceptions
+    # custom method to retrieve a book from the DB and error if it's not found
+    def get_book(self, pk):
+        try:
+            return Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise NotFound(detail="Can't find that book") # <-- import the NotFound exception from rest_framwork.exceptions
 
-#     def get(self, _request, pk):
-#         try:
-#             book = Book.objects.get(pk=pk)
-#             serialized_book = BookSerializer(book)
-#             return Response(serialized_book.data, status=status.HTTP_200_OK)
-#         except Book.DoesNotExist:
-#             raise NotFound(detail="Can't find that book") # <-- import the NotFound exception from rest_framwork.exceptions
+    def get(self, _request, pk):
+        try:
+            book = Book.objects.get(pk=pk)
+            serialized_book = BookSerializer(book)
+            return Response(serialized_book.data, status=status.HTTP_200_OK)
+        except Book.DoesNotExist:
+            raise NotFound(detail="Can't find that book") # <-- import the NotFound exception from rest_framwork.exceptions
 
-#     def put(self, request, pk):
-#         book_to_update = self.get_book(pk=pk)
-#         if book_to_update.owner != request.user:
-#             return Response(status=status.HTTP_401_UNAUTHORIZED)
+    def put(self, request, pk):
+        book_to_update = self.get_book(pk=pk)
+        if book_to_update.owner != request.user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-#         updated_book = BookSerializer(book_to_update, data=request.data)
-#         if updated_book.is_valid():
-#             updated_book.save()
-#             return Response(updated_book.data, status=status.HTTP_202_ACCEPTED)
+        updated_book = BookSerializer(book_to_update, data=request.data)
+        if updated_book.is_valid():
+            updated_book.save()
+            return Response(updated_book.data, status=status.HTTP_202_ACCEPTED)
 
-#         return Response(updated_book.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        return Response(updated_book.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-#     def delete(self, request, pk):
-#         book_to_delete = self.get_book(pk=pk)
+    def delete(self, request, pk):
+        book_to_delete = self.get_book(pk=pk)
 
-#         if book_to_delete.owner != request.user:
-#             return Response(status=status.HTTP_401_UNAUTHORIZED)
+        if book_to_delete.owner != request.user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-#         book_to_delete.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+        book_to_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
